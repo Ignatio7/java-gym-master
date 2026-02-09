4efb0286ea2e7da1bc4225ed30f737b5e4315305
User Ignat <ignacy.sidorov@yandex.ru>
2026-02-09 15:59:52 +0300

    Revert "Merge pull request #2 from Ignatio7/develop"
    This reverts commit 630b3a31f8d2dd35ae227e768e4774d2f5b52cb8, reversing
    changes made to 99d606a5a8d313652a991f6962b1f788152d788e.

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,71 +4,17 @@ import java.util.*;
 
 public class Timetable {
 
-    private Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable;
-
-    public Timetable() {
-        timetable = new HashMap<>();
-    }
+    private /* как это хранить??? */ timetable;
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        DayOfWeek day = trainingSession.getDayOfWeek();
-        TimeOfDay time = trainingSession.getTimeOfDay();
-
-        TreeMap<TimeOfDay, List<TrainingSession>> daySchedule =
-                timetable.computeIfAbsent(day, d -> new TreeMap<>());
-
-        List<TrainingSession> sessionsAtTime =
-                daySchedule.computeIfAbsent(time, t -> new ArrayList<>());
-
-        sessionsAtTime.add(trainingSession);
+        //сохраняем занятие в расписании
     }
 
-    public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(dayOfWeek);
-
-        if (daySchedule == null) {
-            return Collections.emptyList();
-        }
-
-        List<TrainingSession> result = new ArrayList<>();
-
-        for (TimeOfDay time : daySchedule.navigableKeySet()) {
-            result.addAll(daySchedule.get(time));
-        }
-
-        return result;
+    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        //как реализовать, тоже непонятно, но сложность должна быть О(1)
     }
 
-    public List<TrainingSession> getTrainingSessionsForDayAndTime(
-            DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-
-        TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(dayOfWeek);
-
-        if (daySchedule == null) {
-            return Collections.emptyList();
-        }
-
-        return daySchedule.getOrDefault(timeOfDay, Collections.emptyList());
-    }
-
-    public List<CounterOfTrainings> getCountByCoaches() {
-        Map<Coach, Integer> counters = new HashMap<>();
-
-        for (TreeMap<TimeOfDay, List<TrainingSession>> daySchedule : timetable.values()) {
-            for (List<TrainingSession> sessions : daySchedule.values()) {
-                for (TrainingSession session : sessions) {
-                    Coach coach = session.getCoach();
-                    counters.put(coach, counters.getOrDefault(coach, 0) + 1);
-                }
-            }
-        }
-
-        List<CounterOfTrainings> result = new ArrayList<>();
-        for (Map.Entry<Coach, Integer> entry : counters.entrySet()) {
-            result.add(new CounterOfTrainings(entry.getKey(), entry.getValue()));
-        }
-
-        result.sort((a, b) -> Integer.compare(b.getCount(), a.getCount()));
-        return result;
+    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        //как реализовать, тоже непонятно, но сложность должна быть О(1)
     }
 }
